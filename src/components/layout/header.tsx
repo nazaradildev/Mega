e259b4935193b45f6203fbfe592484100c5f4238
Nazar adil <nazaradil.dev@gmail.com>
2025-07-31 15:18:38 +0000

    ان ارتفاعه اكبر من الهدر

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -185,7 +185,7 @@ export function Header() {
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-16 max-w-screen-2xl items-center px-4 md:px-6">
         <Link href="/" className="mr-6 flex items-center space-x-2" prefetch={false}>
-          <Image src="/megalogo11.jpg" alt="MEGA GEOSPATIAL Logo" width={170} height={56} className="rounded-md object-contain" />
+          <Image src="/megalogo11.jpg" alt="MEGA GEOSPATIAL Logo" width={120} height={40} className="rounded-md object-contain" />
           <span className="font-bold hidden sm:inline-block"></span>
         </Link>
 
