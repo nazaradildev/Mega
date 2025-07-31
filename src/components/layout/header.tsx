84209f8b308ae6a98f975faf66242804a2f08e6b
Nazar adil <nazaradil.dev@gmail.com>
2025-07-31 15:15:08 +0000

    مازال صغيرا جدا

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -185,7 +185,7 @@ export function Header() {
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 max-w-screen-2xl items-center px-4 md:px-6">
         <Link href="/" className="mr-6 flex items-center space-x-2" prefetch={false}>
-          <Image src="/megalogo11.jpg" alt="MEGA GEOSPATIAL Logo" width={32} height={32} className="rounded-md" />
+          <Image src="/megalogo11.jpg" alt="MEGA GEOSPATIAL Logo" width={40} height={40} className="rounded-md" />
           <span className="font-bold hidden sm:inline-block">MEGA GEOSPATIAL</span>
         </Link>
 
