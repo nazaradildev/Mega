a28d510c597200dd9329d597004dcc8e7a1d15cd
Nazar adil <nazaradil.dev@gmail.com>
2025-08-02 17:04:09 +0000

    اضف الصورة /home/user/studio/public/blog1.png الي مقال  monitoring-your-

--- src/app/blog/monitoring-your-property-from-space/page.tsx
+++ src/app/blog/monitoring-your-property-from-space/page.tsx
@@ -14,7 +14,7 @@ export default function BlogPostPage() {
              </h1>
              <div className="relative aspect-video">
                 <Image 
-                    src="https://placehold.co/1200x675.png" 
+                    src="/blog1.png" 
                     alt="Satellite view of property changes"
                     fill
                     sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
